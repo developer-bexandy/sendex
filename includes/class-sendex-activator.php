de5e7c2eba0437d37bc49efd2745e305bf345a68
Bexandy Rodriguez Desktop <brodriguez@bexandyrodriguez.com.ve>
2019-02-12 16:32:20 -0400

    Primer Commit

---
+++ includes/class-sendex-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bexandyrodriguez.com.ve
+ * @since      1.0.0
+ *
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ * @author     Bexandy Rodriguez <developer@bexandyrodriguez.com.ve>
+ */
+class Sendex_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-sendex-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bexandyrodriguez.com.ve
+ * @since      1.0.0
+ *
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ * @author     Bexandy Rodriguez <developer@bexandyrodriguez.com.ve>
+ */
+class Sendex_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
